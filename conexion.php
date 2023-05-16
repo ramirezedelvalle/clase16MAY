c3ac23c7fc49d36b72232e9824e5e051d40008a2
Nozen <ramirezedelvalle@gmail.com>
2023-05-16 16:59:00 -0600

    conexion

---
+++ conexion.php
@@ -0,0 +1,13 @@
+<?php
+
+try {
+    $conexion = new PDO("informix:host=host.docker.internal; service=9088;database=mdn; server=informix; protocol=onsoctcp;EnableScrollableCursors=1", "informix", "in4mix");
+    echo "conexión exitosa";
+} catch (PDOException $e) {
+    echo "NO SE CONECTO A LA BD";
+    echo "<br>";
+    echo $e->getMessage();
+}
+echo "<br>";
+echo "mensaje despues de la conexión";
+?>
